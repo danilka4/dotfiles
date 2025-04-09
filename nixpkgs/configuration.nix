237931e81db14be0ecbc3514c047277991a7cac9
daniel pnix <danielpalamarchuk@gmail.com>
2025-04-09 11:16:40 -0400

    Adding nonsense to configuration.nix

---
+++ nixpkgs/configuration.nix
@@ -0,0 +1,165 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports = [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+  # Switches caps to escape
+  services.kmonad = { enable = true; };
+  services.xserver.xkb.options = "caps:escape";
+
+  environment.variables.EDITOR = "nvim";
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  programs.zsh.enable = true;
+  users.users.lizzy = {
+    isNormalUser = true;
+    description = "Daniel Palamarchuk";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [ ];
+    shell = pkgs.zsh;
+  };
+
+  # Enable automatic login for the user.
+  services.getty.autologinUser = "lizzy";
+
+  # Allow unfree packages and unstable
+  nixpkgs.config = {
+    allowUnfree = true;
+    packageOverrides = pkgs: {
+      unstable = import <unstable> { config = config.nixpkgs.config; };
+    };
+  };
+  # Enables fonts
+  fonts.fontDir.enable = true;
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+
+  environment.systemPackages = with pkgs; [
+    git
+    xclip
+    vim
+    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    alacritty
+    librewolf
+    wget
+    discord
+    obsidian
+    gh
+    polybar
+    zathura
+    R
+    zoxide
+    zsh
+    lsd
+    hyfetch
+    htop
+    gcc
+    gnumake
+    go
+    flameshot
+    texlive.combined.scheme-full
+    pandoc
+    nerd-fonts.fira-code
+    lazygit
+    rustup
+
+    lua-language-server
+    ltex-ls
+    nil
+    texlab
+    nixfmt-classic
+    tree-sitter
+  ];
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.fira-code
+    nerd-fonts.droid-sans-mono
+    noto-fonts-emoji
+  ];
+
+  environment.pathsToLink = [ "/libexec" ];
+
+  services.picom.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+  services.displayManager.defaultSession = "none+i3";
+  services.xserver = {
+    enable = true;
+    desktopManager = { xterm.enable = false; };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [ feh dmenu i3status i3lock i3blocks picom ];
+    };
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
